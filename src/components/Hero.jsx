84c3982ec361c3fdb411b885a739a8dd917f6c87
Seasites <seasites10@gmail.com>
2024-01-08 12:17:06 +0530

    search syntax

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,11 @@ const Hero = () => {
     const onSubmitHandle = async (e) => {
         e.preventDefault();
         let inputValue = document.getElementById("default-search").value;
-
+        let searchValue = inputValue.replace(' ', '+')
+        console.log(searchValue)
         toast.success('Successfully toasted!', "bottom-center")
         try {
-            const response = await fetch(`${BASE_URL}/?s=${inputValue}&apikey=ac3a2822`, {
+            const response = await fetch(`${BASE_URL}/?s=${searchValue}&apikey=ac3a2822`, {
                 method: "GET",
             });
 
@@ -63,44 +64,57 @@ const Hero = () => {
             </div>
             <div className='lg:max-w-7xl w-full mx-4 my-10 lg:m-0 lg:mx-auto'>
 
-
-
                 <div className="p-4 md:p-5 ">
-
-                    <div className="grid my-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-                        {   
-
-                            
-                            results.map((movie, index) => {
-
-                                return (
-                                    <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
-                                        <a href="#">
-                                            <img className="rounded-t-lg w-full h-96" src={movie.Poster} alt={movie.Title} />
-                                        </a>
-                                        <div className="p-5">
-                                            <a href="#">
-                                                <h5 className=" text-2xl font-bold tracking-tight text-gray-900 ">{movie.Title}</h5>
-                                                <p className='mb-2 text-sm text-gray-500'>{movie.Type}</p>
-                                            </a>
-                                            <p className="mb-3 font-normal text-gray-700 ">{movie.Plot}</p>
-                                            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
-                                                Read more
-                                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                                </svg>
-                                            </a>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
+                    {
+                        results ? (
+                            <>
+                                <div className="grid my-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+                                    {
 
 
+                                        results.map((movie, index) => {
+
+                                            return (
+                                                <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+                                                    <a href="#">
+                                                        <img className="rounded-t-lg w-full h-96" src={movie.Poster} alt={movie.Title} />
+                                                    </a>
+                                                    <div className="p-5">
+                                                        <a href="#">
+                                                            <h5 className=" text-2xl font-bold tracking-tight text-gray-900 ">{movie.Title}</h5>
+                                                            <p className='mb-2 text-sm text-gray-500 capitalize'>{movie.Type}</p>
+                                                        </a>
+                                                        <p className="mb-3 font-normal text-gray-700 capitalize">{movie.Plot}</p>
+                                                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                                                            Read more
+                                                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                                            </svg>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
 
 
-                    </div>
 
+
+                                </div>
+
+
+
+                            </>
+                        ) : (
+                            <div className='rounded-md shadow-sm flex items-center justify-center
+                            border border-gray-300 bg-white py-2 px-6
+                            text-base font-medium text-gray-900 hover:text-gray
+                            -900 focus:ring ring-indigo-500 focus:border
+                            border-transparent cursor-wait transition ease-
+                            medium duration-150 sm:py-4 sm:text-base'>Loading
+                                ...</div>
+                        )
+                    }
 
 
                 </div>
